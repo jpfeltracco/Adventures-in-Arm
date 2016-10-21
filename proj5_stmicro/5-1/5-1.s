c07c1b4fef2d63abf716703d33df7dd9312d42d1
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-10-21 17:15:43 -0400

    Added Answers file and completed assignment 1.

--- proj5_stmicro/5-1/5-1.s
+++ proj5_stmicro/5-1/5-1.s
@@ -8,10 +8,18 @@ __Vectors
   DCD Reset_Handler + 1  ; Entry point (+1 for thumb mode)
 
 Reset_Handler
+GREEN EQU 0x200
+BLUE  EQU 0x100
+BOTH  EQU 0x300
+
+SHORT EQU 0x200000
+LONG  EQU 0x400000
+LETTERPAUSE EQU 0x600000
+
   ; Your code here
   ; Clock enable
   ldr r0, =0x40021018    ; RCC_APB2ENR register
-  mov r1, #0x10           ; set bit 4
+  mov r1, #0x10          ; set bit 4
   str r1, [r0]           ; store back in RCC_APB2ENR
   
   ; Set outputs (GPIOC_CRH)
@@ -19,26 +27,82 @@ Reset_Handler
   mov r1, #0x22          ; low-speed push-pull ouputs on bits 8/9
   str r1, [r0]
   
-  ldr r0, =0x4001100c ; GPIOC_ODR
-loop
-  mov r1, #0x300
-  str r1, [r0]
-  mov r2, #0x80000
-delay0
-  sub r2, #1
-  cmp r2, #0
-  bne delay0
-  
-  mov r1, #0
-  str r1, [r0]
-  
-  mov r2, #0x80000
-delay1
-  sub r2, #1
-  cmp r2, #0
-  bne delay1
-  
-  b loop
+  ldr r1, =0x4001100c   ; PORTC->odr; see RM0041 p. 113
 
+atl
+  ldr r0, =SHORT
+  ldr r3, =GREEN
+  bl blink  
+  
+  ldr r0, =LONG
+  ldr r3, =BLUE
+  bl blink
+  
+  ldr r0, =LETTERPAUSE
+  bl delay
+  
+  ldr r0, =LONG
+  ldr r3, =BLUE
+  bl blink
+  
+  ldr r0, =LETTERPAUSE
+  bl delay
+  
+  ldr r0, =SHORT
+  ldr r3, =GREEN
+  bl blink
+  
+  ldr r0, =LONG
+  ldr r3, =BLUE
+  bl blink
+
+  ldr r0, =SHORT
+  ldr r3, =GREEN
+  bl blink
+  
+  ldr r0, =SHORT
+  ldr r3, =GREEN
+  bl blink
+
+  ldr r0, =LETTERPAUSE
+  bl delay
+  
+  b atl
+
+
+; r0: length of blink
+; r1: address of odr to write to
+; r3: data to write to odr
+blink
+  push {r0, r3, lr} ; Push; see RM0041 p. 68
+  ldr r1, =0x4001100C   ; PORTC->odr; see RM0041 p. 113
+
+  str r3, [r1]
+
+  bl delay
+
+  ldr r3, =0
+  str r3, [r1]
+
+  
+bfinish
+  ldr r0, =0x100000 ; delay between blinks
+  bl delay
+
+  ; We push lr and pop it into the pc as our procedure return.
+  pop {r0, r3, pc}
+
+delay
+  push {r0}
+        
+dloop
+  cbz r0, dfinish
+  sub r0, #1
+  b dloop ; Unconditional jump; PM0056 p. 92
+        
+dfinish
+  pop {r0}
+  bx lr
+  
   align
   END
