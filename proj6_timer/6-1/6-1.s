cb36d2eb28d964c471df187021792af281489c40
Jeremy Feltracco <jpfeltracco@gatech.edu>
2016-11-04 17:37:06 -0400

    Working implementation of the first problem.

--- proj6_timer/6-1/6-1.s
+++ proj6_timer/6-1/6-1.s
@@ -8,22 +8,21 @@ __Vectors
   DCD Reset_Handler + 1   ; Entry point (+1 for thumb mode)
   DCD 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
   DCD systick_handler + 1 ; 
-  DCD 0, 0, 0, 0, 0, 0 
-  DCD ext0_handler + 1    ; IRQ6 : external interrupt from exti controller
+  ;DCD 0, 0, 0, 0, 0, 0 
+  ;DCD ext0_handler + 1    ; IRQ6 : external interrupt from exti controller
 
 Reset_Handler
-COUNT EQU   0x20000400
-SIMUL EQU   0x20000408
-PRESSED EQU 0x2000040C
-OLDCNT EQU  0x20000414
+COUNT   equ 0x20000400
+OLDCNT  equ 0x20000404
+PRESSED equ 0x20000408
+SIMUL   equ 0x2000040C
+BREAK   equ 0x20000410         ; indicates that we should break out of delay
 
-GREEN EQU 0x200              ; Address in ODR to write to
-BLUE  EQU 0x100
-BOTH  EQU 0x300
+GREEN   equ 0x200              ; Address in ODR to write to
+BLUE    equ 0x100
+BOTH    equ 0x300
 
-SHORT EQU 0xF000             ; Number of iterations
-LONG  EQU 0x400000
-LETTERPAUSE EQU 0x600000
+SHORT   equ 0xFF00             ; Number of iterations
 
         ;  Enable I/O port clocks 
         ldr r0, =0x40021018 ; RCC->apb2enr, see RM0041 p. 84 
@@ -42,37 +41,13 @@ LETTERPAUSE EQU 0x600000
         ldr r1, =0x44444444 ; All inputs; RM0041 p. 112 
         str r1, [r0]
 
-        ; Set EXTI0 source to Port A 
-        ldr r0, =0x40010008 ; AFIO->exticr1, see RM0041 p. 124 
-        ldr r1, [r0]
-        bic r1, #0xf        ; Mask out the last 4 bits (set to 0, Port A) 
-        str r1, [r0]
-
         ; Initialize the systick timer to branch to systick every 1 millisecond
         ldr r0, =0xe000e010 ; systick base; PM0056 p. 150 
-        ldr r1, =500000   ; systick reload value; PM0056 p. 152 
+        ldr r1, =1000   ; systick reload value; PM0056 p. 152 
         str r1, [r0, #4]  ; SYSTICK->load 
         ldr r1, [r0, #0]  ; SYSTICK->ctrl; PM0056 p. 151 
-        orr r1, #3        ; Set to 3: enable interrupt and counting 
+        orr r1, #7        ; Set to 3: enable interrupt and counting 
         str r1, [r0, #0]
-        
-        ; Set up interrupt on rising edge of port A bit 0 on the EXTI (external
-        ; interrupt controller); see RM0041 p. 134
-        ldr r0, =0x40010400 ; EXTI base address 
-        mov r1, #1
-        str r1, [r0, #8]    ; EXTI->rtsr; event 0 rising; see RM0041 p. 139 
-        str r1, [r0, #0xC]  ; EXTI->ftsr; falling
-        str r1, [r0, #0]    ; EXTI->imr; unmask line 0 
-
-        ; Set up the IRQ in the NVIC. See PM0056 p. 118
-        ldr r0, =0xe000e404 ; Address of NVIC->ipr1; PM0056 p. 128 
-        ldr r1, [r0]        ; NVIC->ipr1; PM0056 p. 125 
-        bic r1, #0xff0000   ; Zero out bits corresponding to IRQ6 
-        str r1, [r0]        ; Set IRQ6 priority to 0 (highest) 
-        
-        ldr r0, =0xe000e100 ; NVIC->iser0; PM0056 p. 120 
-        mov r2, #0x40       ; Bit corresponding to IRQ6 
-        str r2, [r0]        ; NVC->iser0; set enabled 
         
         mov r0, #0
         ldr r1, =SIMUL
@@ -88,9 +63,14 @@ LETTERPAUSE EQU 0x600000
         str r0, [r1]
 
         ; Enable interrupts. 
-        cpsie i
+        ;cpsie i
 
 loop    
+        ; Reset break flag
+        mov r0, #0
+        ldr r1, =BREAK
+        str r0, [r1]
+        
         ; Check if SIMUL is enabled
         ldr r1, =SIMUL
         ldr r1, [r1]
@@ -99,144 +79,140 @@ loop
 simul_dis
         ; SIMUL not enabled, flash alternating
         ldr r0, =SHORT             ; Do alternating blinking.
-        ldr r3, =GREEN
+        ldr r1, =GREEN
         bl blink
         ldr r0, =SHORT
-        ldr r3, =BLUE
+        ldr r1, =BLUE
         bl blink
         b loop
 simul_enb
         ; SIMUL is enabled, flash same time
         ldr r0, =SHORT             ; Do simultaneous blinking.
-        ldr r3, =BOTH
+        ldr r1, =BOTH
         bl blink
         b loop
-        
-; Checks button state for use in conditionals, sets Z-bit
-; if equal, and clears Z-bit if not equal.
-button
-        push {r0}
-        ldr r0, =0x40010808        ; Memory address for reading GPIOA_IDR
+
+; Returns button state in r0
+buttonval
+        push {lr}
+        ldr r0, =0x40010808
         ldr r0, [r0]
-        ands r0, #1                ; Only care about bit 0
-        pop {r0}
-        bx lr
-
-
-ext0_handler  push {lr, r0, r1}
-
-        ; Skip the handler if the button is no longer pressed. 
-        ;ldr r0, =0x40010808
-        ;ldr r0, [r0]
-        ;and r0, #1 ; Test bit 0 
-        ;cbz r0, skip_ext0
-        ldr r1, =PRESSED
-        ldr r1, [r1]
-        cmp r1, #0
-        bne ext0_unpressed
-ext0_pressed
-        bl button
-        beq ext0_done ; button isn't actually pressed, escape
-        ; Clear counter
-        mov r0, #0
-        ldr r1, =COUNT
-        str r0, [r1]
-        ; Set PRESSED to TRUE
-        mov r0, #1
-        ldr r1, =PRESSED
-        str r0, [r1]
-        b ext0_done
-ext0_unpressed
-        bl button
-        bne ext0_done
-        ; PRESSED = false
-        mov r0, #0
-        ldr r1, =PRESSED
-        str r0, [r1]
-        ; Get COUNTER Value
-        ldr r1, =COUNT
-        ldr r1, [r1]
-        ; Store in OLDCNT
-        ldr r0, =OLDCNT
-        str r1, [r0]
-        ; Set SIMUL to ON
-        mov r0, #1
-        ldr r1, =SIMUL
-        str r0, [r1]
-
-ext0_done
-        ; Clear pending-bit in EXTI; see RM0041 p. 140
-        ldr r0, =0x40010414
-        mov r1, #1
-        str r1, [r0]
-
-        ; Clear pending-bit in interrupt controller
-        ldr r0, =0xE000E280        ; NVIC->icpr0; see PM0051 p. 123
-        mov r1, #0x40
-        str r1, [r0]
-        
-        pop {pc, r0, r1} ; Return from interrupt. 
-
+        and r0, #1
+        pop {pc}
 
 systick_handler
         push {lr, r0, r1, r2}
 
-        ; Increment counter
+        bl buttonval
+        ; r0 is now button val
+        ldr r1, =PRESSED
+        ldr r1, [r1]
+        
+        cmp r0, r1    ; compare current button state with last button
+        beq systick_end
+        
+        ; We had a button transition, check if its a press or unpress
+        cmp r0, #0
+        beq unpressed
+        ; beq means unpressed
+        ; bne means pressed
+pressed
+        ; set pressed to true
+        mov r0, #1
+        ldr r1, =PRESSED
+        str r0, [r1]
+        ; clear count
+        mov r0, #0
+        ldr r1, =COUNT
+        str r0, [r1]
+        ; done
+        b systick_done
+unpressed
+        ; set pressed to false
+        mov r0, #0
+        ldr r1, =PRESSED
+        str r0, [r1]
+        ; store count in old count
+        ldr r0, =COUNT
+        ldr r0, [r0]
+        ldr r1, =OLDCNT
+        str r0, [r1]
+        ; set simul to true
+        mov r0, #1
+        ldr r1, =SIMUL
+        str r0, [r1]
+        ; clear count
+        mov r0, #0
+        ldr r1, =COUNT
+        str r0, [r1]
+        ; set break to true to exit alternating blinking
+        mov r0, #1
+        ldr r1, =BREAK
+        str r0, [r1]
+        ; done
+        b systick_done
+systick_end
+        ; increment count
         ldr r0, =COUNT
         ldr r1, [r0]
         add r1, #1
         str r1, [r0]
-        
-        ; Get simul mode var
-        ldr r2, =SIMUL
-        ldr r2, [r2]
-        
-        cmp r2, #0
-        beq systick_end
-        ; Get old counter var
-        ldr r0, =OLDCNT
-        ldr r0, [r0]
-        ; Compare old counter and counter
-        cmp r0, r1
-        ; If old counter > counter, don't disable simul
-        bge systick_end
+        ; check simul
+        ldr r1, =SIMUL
+        ldr r1, [r1]
         mov r0, #0
-        ; Set simul mode to 0 (false)
-        ldr r2, =SIMUL
-        str r0, [r2]
-
-systick_end
+        cmp r0, r1
+        beq systick_done
+        ; simul is true, so check if we're done
+        ldr r0, =COUNT
+        ldr r0, [r0]
+        ldr r1, =OLDCNT
+        ldr r1, [r1]
+        cmp r0, r1
+        ble systick_done
+        ; count exceeds old count, so we're done with simul
+        mov r0, #0
+        ldr r1, =SIMUL
+        str r0, [r1]
+        ; set break to true to exit simul mode immediately
+        mov r0, #1
+        ldr r1, =BREAK
+        str r0, [r1]
+        ; done
+        
+systick_done
         pop {pc, r0, r1, r2}
         
-        ; Takes number of delay iterations in r0. 
-delay   push {r0}
+; r0, length of delay
+delay   push {lr, r0, r1}
         
-dloop   cbz r0, dfinish
+dloop   ldr r1, =BREAK
+        ldr r1, [r1]
+        cmp r1, #0
+        bne dfinish
+        cbz r0, dfinish
         sub r0, #1
         b dloop     ; Unconditional jump; PM0056 p. 92 
         
-dfinish pop {r0}
-        bx lr
+dfinish pop {pc, r0, r1}
         
         
 ; r0: length of blink
-; r3: data to write to odr
+; r1: data to write to odr
 blink
-        push {r0, r1, r3, lr}          ; Push; see RM0041 p. 68
-        ldr r1, =0x4001100C        ; PORTC->odr; see RM0041 p. 113
-
-        str r3, [r1]
-
+        push {r0, r1, r2, lr}      ; Push; see RM0041 p. 68
+        ldr r2, =0x4001100C        ; PORTC->odr; see RM0041 p. 113
+        str r1, [r2]
+        ; uses r0 for delaying
         bl delay
-
-        ldr r3, =0
-        str r3, [r1]
-
+        
+        mov r0, #0
+        str r0, [r2]
 bfinish
-        ldr r0, =0x100000          ; delay between blinks
+        ldr r0, =0x50000           ; delay between blinks
         bl delay
 
-        pop {r0, r1, r3, pc}           ; pop pc as procedure return (from lr)
+        pop {r0, r1, r2, pc}       ; pop pc as procedure return (from lr)
 
   align
   END
